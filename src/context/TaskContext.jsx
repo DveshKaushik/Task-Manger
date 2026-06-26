96254e4cca668693bf18c7f284da679d2f77eb5b
DveshKaushik <dveshkaushik@email.com>
2026-06-26 11:30:47 +0530

    f

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,9 +1,25 @@
-import { createContext, useState } from "react";
+import {
+  createContext,
+  useState,
+  useEffect
+} from "react";
 
 export const TaskContext = createContext();
 
 export const TaskProvider = ({ children }) => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(() => {
+    const savedTasks =localStoragae.getitem("Task");
+    return savedTasks 
+    ? JSON.parse(savedTasks)
+    :[];
+  })
+  
+  useEffect(() => {
+  localStorage.setItem(
+    "tasks",
+    JSON.stringify(tasks)
+  );
+}, [tasks]);;
 
   const addTask = (task) => {
     setTasks([...tasks, task]);
